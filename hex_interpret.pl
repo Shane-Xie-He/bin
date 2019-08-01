23a4c7af8314f61f07cf1698f7859f65fadad9dd
Shane He <19219874+Shane-Xie-He@users.noreply.github.com>
2019-07-31 23:47:23 -0700

    Prefer single quotes

--- hex_interpret.pl
+++ hex_interpret.pl
@@ -4,9 +4,9 @@ use strict;
 use warnings;
 
 my $len = $ARGV[0];
-$len = "" if ! defined $len;
+$len = '' if ! defined $len;
 
 while (<STDIN>) {
 	chomp;
-	printf "%0".$len."b\n", hex $_
+	printf '%0'.$len."b\n", hex $_
 }
